c485cc9a85a630f687c1f7fd405a06b12948e8e5
Fabian Feitsch <info@fafeitsch.de>
2019-06-08 15:45:17 +0200

    interim commit towards pseudomymify

--- samples/pseudonymify.go
+++ samples/pseudonymify.go
@@ -1,54 +1,113 @@
 package samples
 
 import (
+	"errors"
+	"fmt"
 	"github.com/fafeitsch/open-callopticum/cdrcsv"
+	"sort"
 	"strings"
+	"time"
 )
 
-func pseudoymify(cdrs []cdrcsv.File, contacts []SampleContact) []cdrcsv.Record {
-	return nil
+type TimeShifter interface {
+	shiftTime(time.Time) time.Time
 }
 
-type participant struct {
-	name      string
-	extension string
+type IdentityTimeShifter struct {
 }
 
-func findParticipants(cdrs []cdrcsv.File) []participant {
-	//TODO: Document assumption that an extension is bound to at most one name
-	result := make([]participant, 0)
+func (i *IdentityTimeShifter) shiftTime(time time.Time) time.Time {
+	return time
+}
+
+func pseudoymify(cdrs []cdrcsv.File, pseudoContacts []Participant, pseudoContexts []string) error {
+	participants := findParticipants(cdrs)
+	if len(pseudoContacts) < len(participants) {
+		return errors.New(fmt.Sprintf("number of pseudo contacts is not sufficient, at least %d are needed, only %d were provided", len(participants), len(pseudoContacts)))
+	}
+	contexts := findContexts(cdrs)
+	if len(pseudoContexts) < len(contexts) {
+		return errors.New(fmt.Sprintf("number of pseudo contexts is not sufficient, at least %d are needed, only %d were provided", len(contexts), len(pseudoContexts)))
+	}
+	participantMapping := make(map[Participant]Participant)
+	for index, participant := range participants {
+		participantMapping[participant] = pseudoContacts[index]
+	}
+	contextMapping := make(map[string]string)
+	for index, context := range contexts {
+		contextMapping[context] = pseudoContexts[index]
+	}
+	for _, file := range cdrs {
+		for _, record := range file.Records {
+			_ = callerIdToParticipant(record.CallerId)
+		}
+	}
+}
+
+func callerIdToParticipant(callerId string) Participant {
+	firstBracket := strings.Index(callerId, "<")
+	extension := callerId[firstBracket+1 : len(callerId)-1]
+	name := callerId[1 : firstBracket-2]
+	return Participant{Name: name, Extension: extension}
+}
+
+type Participant struct {
+	Name      string
+	Extension string
+}
+
+func findParticipants(cdrs []cdrcsv.File) []Participant {
+	//TODO: Document assumption that an Extension is bound to at most one Name
+	result := make([]Participant, 0)
 	distinctExtensions := make(map[string]bool)
 	distinctNames := make(map[string]bool)
 	for _, cdr := range cdrs {
 		for _, record := range cdr.Records {
-			callerId := record.CallerId
-			firstBracket := strings.Index(callerId, "<")
-			extension := callerId[firstBracket+1 : len(callerId)-1]
-			name := callerId[1 : firstBracket-2]
-			if _, ok := distinctExtensions[extension]; ok {
+			participant := callerIdToParticipant(record.CallerId)
+			if _, ok := distinctExtensions[participant.Extension]; ok {
 				continue
 			}
-			if _, ok := distinctNames[name]; ok {
+			if _, ok := distinctNames[participant.Name]; ok {
 				continue
 			}
-			distinctExtensions[extension] = true
-			distinctNames[name] = true
-			result = append(result, participant{name: name, extension: extension})
+			distinctExtensions[participant.Extension] = true
+			distinctNames[participant.Name] = true
+			result = append(result, participant)
 		}
 		for _, record := range cdr.Records {
 			if _, ok := distinctExtensions[record.Src]; ok {
 				continue
 			}
 			distinctExtensions[record.Src] = true
-			result = append(result, participant{name: "", extension: record.Src})
+			result = append(result, Participant{Name: "", Extension: record.Src})
 		}
 		for _, record := range cdr.Records {
 			if _, ok := distinctExtensions[record.Dst]; ok {
 				continue
 			}
 			distinctExtensions[record.Dst] = true
-			result = append(result, participant{name: "", extension: record.Dst})
+			result = append(result, Participant{Name: "", Extension: record.Dst})
 		}
 	}
+	sort.Slice(result, func(i, j) bool {
+		return result[i].Name < result[j].Name
+	})
+	return result
+}
+
+func findContexts(cdrs []cdrcsv.File) []string {
+	result := make([]string, 0)
+	set := make(map[string]bool)
+	for _, file := range cdrs {
+		for _, record := range file.Records {
+			set[record.Dcontext] = true
+		}
+	}
+	for key, value := range set {
+		if value {
+			result = append(result, key)
+		}
+	}
+	sort.Strings(result)
 	return result
 }
--- samples/pseudonymify_test.go
+++ samples/pseudonymify_test.go
@@ -12,28 +12,58 @@ func TestFindParticipants(t *testing.T) {
 		t.Errorf("could not open the test cdr: %v", err)
 		return
 	}
+	defer func() {
+		_ = reader.Close()
+	}()
 	file, err := cdrcsv.ReadWithoutHeader(reader)
 	if err != nil {
 		t.Errorf("could not parse test cdr: %v", err)
 		return
 	}
 	participants := findParticipants([]cdrcsv.File{*file})
-	expectedParticipants := []participant{
-		{name: "Judith Queston", extension: "15"},
-		{name: "John Doe", extension: "12"},
-		{name: "", extension: "0815"},
-		{name: "", extension: "0923526333"},
+	expectedParticipants := []Participant{
+		{Name: "Judith Queston", Extension: "15"},
+		{Name: "John Doe", Extension: "12"},
+		{Name: "", Extension: "0815"},
+		{Name: "", Extension: "0923526333"},
 	}
 	if len(participants) != len(expectedParticipants) {
 		t.Errorf("Expected participants were %d, but actual were %d", len(expectedParticipants), len(participants))
 		return
 	}
 	for index, participant := range participants {
-		if participant.name != expectedParticipants[index].name {
-			t.Errorf("Expected name of participant %d is %s, but was %s", index, expectedParticipants[index].name, participant.name)
+		if participant.Name != expectedParticipants[index].name {
+			t.Errorf("Expected Name of participant %d is %s, but was %s", index, expectedParticipants[index].name, participant.Name)
 		}
-		if participant.extension != expectedParticipants[index].extension {
-			t.Errorf("Expected extension of participant %d is %s, but was %s", index, expectedParticipants[index].extension, participant.extension)
+		if participant.Extension != expectedParticipants[index].extension {
+			t.Errorf("Expected Extension of participant %d is %s, but was %s", index, expectedParticipants[index].extension, participant.Extension)
+		}
+	}
+}
+
+func TestFindContexts(t *testing.T) {
+	reader, err := os.Open("create_test_cdr.csv")
+	if err != nil {
+		t.Errorf("could not open the test cdr: %v", err)
+		return
+	}
+	defer func() {
+		_ = reader.Close()
+	}()
+	file, err := cdrcsv.ReadWithoutHeader(reader)
+	if err != nil {
+		t.Errorf("could not parse test cdr: %v", err)
+		return
+	}
+	contexts := findContexts([]cdrcsv.File{*file})
+	expectedContexts := []string{"external", "internal"}
+	if len(contexts) != len(expectedContexts) {
+		t.Errorf("Expected contexts were %d, but actual contexts were %d.", len(expectedContexts), len(contexts))
+		return
+	}
+	for index, context := range contexts {
+		if context != expectedContexts[index] {
+			t.Errorf("Context at location %d should be %s, but was %s", index, expectedContexts[index], context)
 		}
 	}
 }
