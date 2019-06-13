a4e6ed278837f69f2adac276c9d683d2cda6a9d3
Fabian Feitsch <info@fafeitsch.de>
2019-06-13 20:39:05 +0200

    implement parsing of definitions, matchers a not yet tested

--- report/persistency.go
+++ report/persistency.go
@@ -4,7 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/fafeitsch/open-callopticum/cdrcsv"
 	"io"
+	"reflect"
+	"strings"
 )
 
 type jsonFormula struct {
@@ -37,5 +40,55 @@ func ParseDefinition(reader io.Reader) (ReportDefinition, error) {
 	if err != nil {
 		return ReportDefinition{}, fmt.Errorf("could not parse json: %v", err)
 	}
-	return ReportDefinition{}, nil
+	return convertJson(jsonReport)
+}
+
+func convertJson(report jsonReport) (ReportDefinition, error) {
+	countings := make([]CountingsDefinition, 0, len(report.Countings))
+	for index, jsonCounting := range report.Countings {
+		matcher, err := convertFormula(jsonCounting.Formula)
+		if err != nil {
+			return ReportDefinition{}, fmt.Errorf("parsing the formula of the %dth counting failed: %v", index, err)
+		}
+		counting := CountingsDefinition{Name: jsonCounting.Name, DisplayName: jsonCounting.DisplayName, ExcludeOtherMatches: jsonCounting.ExludeOtherMatches, Formula: matcher}
+		countings = append(countings, counting)
+	}
+	return ReportDefinition{Countings: countings}, nil
+}
+
+func convertFormula(formula jsonFormula) (Matcher, error) {
+	if formula.Left != nil && formula.Right != nil && formula.Operator != "" && formula.Regex == "" && formula.Column == "" {
+		leftFormula, err := convertFormula(*formula.Left)
+		if err != nil {
+			return nil, fmt.Errorf("could not parse left formula: %v", err)
+		}
+		rightFormula, err := convertFormula(*formula.Right)
+		if err != nil {
+			return nil, fmt.Errorf("could not parse right formula: %v", err)
+		}
+		if formula.Operator == "and" || formula.Operator == "&&" {
+			return &AndMatcher{left: leftFormula, right: rightFormula}, nil
+		}
+		if formula.Operator == "or" || formula.Operator == "||" {
+			return &OrMatcher{left: leftFormula, right: rightFormula}, nil
+		}
+		return nil, fmt.Errorf("Operator '%s' not known. Use one of 'and', '&&', 'or' or '||'.", formula.Operator)
+	}
+	if formula.Left == nil && formula.Right == nil && formula.Operator == "" && formula.Regex != "" && formula.Column != "" {
+		refType := reflect.TypeOf(cdrcsv.Record{})
+		_, ok := refType.FieldByName(formula.Column)
+		if !ok {
+			fields := make([]string, 0, refType.NumField())
+			for i := 0; i < refType.NumField(); i++ {
+				fields = append(fields, refType.Field(i).Name)
+			}
+			return nil, fmt.Errorf("Column '%s' not found. Column must be one of: %s", formula.Column, strings.Join(fields, ", "))
+		}
+		provider := func(record cdrcsv.Record) string {
+			val := reflect.ValueOf(record)
+			return val.FieldByName(formula.Column).String()
+		}
+		return &RegexMatcher{provider: provider}, nil
+	}
+	return nil, fmt.Errorf("either left, right and operator fields must be set or regex and column fields must be set.")
 }
--- report/persistency_test.go
+++ report/persistency_test.go
@@ -21,17 +21,17 @@ func TestParseDefinition(t *testing.T) {
 		t.Errorf("Expected number of parsed countings should be 2 but was %d.", len(reportDefinition.Countings))
 	}
 	expectedNames := []string{"production_calls", "after_business"}
-	expectedDisplayNames := []string{"Calls from the Production Department", "Calls after business hours"}
-	expectedExclude := []bool{true, false}
+	expectedDisplayNames := []string{"Calls from the Production Department", "Calls from the headquarter after business hours"}
+	expectedExclude := []bool{false, true}
 	for index, counting := range reportDefinition.Countings {
 		if expectedNames[index] != counting.Name {
-			t.Errorf("Expected name of the %d.th counting is %s but was %s.", index, expectedNames[index], counting.Name)
+			t.Errorf("Expected name of the %dth counting is '%s' but was '%s.'", index, expectedNames[index], counting.Name)
 		}
 		if expectedDisplayNames[index] != counting.DisplayName {
-			t.Errorf("Expected display name of the %d.th counting is %s but was %s.", index, expectedDisplayNames[index], counting.DisplayName)
+			t.Errorf("Expected display name of the %dth counting is '%s' but was '%s'.", index, expectedDisplayNames[index], counting.DisplayName)
 		}
 		if expectedExclude[index] != counting.ExcludeOtherMatches {
-			t.Errorf("Expected 'exclude_other_matchings' of %d.th counting is %t but was %t.", index, expectedExclude[index], counting.ExcludeOtherMatches)
+			t.Errorf("Expected 'exclude_other_matchings' of %dth counting is %t but was %t.", index, expectedExclude[index], counting.ExcludeOtherMatches)
 		}
 	}
 }
